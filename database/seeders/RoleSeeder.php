8ff092b8de8a2f52c2a974d19f91b6eeb5b3695c
Adu Lawrence <aduaondohemba@gmail.com>
2023-08-11 15:51:37 +0100

    roles and permissions seeders and migration

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Domains\Auth\PermissionTypes;
+use App\Domains\Auth\RoleTypes;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = collect([
+            RoleTypes::ADMINISTRATOR,
+            RoleTypes::ASSET_MANAGER,
+            RoleTypes::TECHNICIAN,
+            RoleTypes::FINANCE,
+            RoleTypes::VIEWER
+        ]);
+
+        $roles->each(function($role){
+            $dbRole = Role::firstOrCreate(['name' => $role->value]);
+
+            if($role == RoleTypes::ADMINISTRATOR){
+                $permissions = Permission::all();
+
+                $dbRole->syncPermissions($permissions);
+            }
+        });
+    }
+}
