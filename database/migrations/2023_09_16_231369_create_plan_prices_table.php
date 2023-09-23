2dd988150815587482e34e94273a78d347389bad
chidi <chidi.nkwocha@rayda.co>
2023-09-23 10:33:39 +0100

    Update 2023_09_16_231369_create_plan_prices_table.php

--- database/migrations/2023_09_16_231369_create_plan_prices_table.php
+++ database/migrations/2023_09_16_231369_create_plan_prices_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Domains\Constant\CommonConstant;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,7 +20,7 @@ return new class extends Migration
         Schema::create('plan_prices', function (Blueprint $table) {
             $table->uuid(PlanPriceConstant::ID)->unique()->primary();
             $table->foreign(PlanPriceConstant::PLAN_ID)->references(PlanConstant::ID)->on(Plan::getTableName());
-            $table->foreign(PlanPriceConstant::CURRENCY_CODE)->references(\App\Domains\Constant\CommonConstant::CODE)->on(Currency::getTableName());
+            $table->foreign(PlanPriceConstant::CURRENCY_CODE)->references(CommonConstant::CODE)->on(Currency::getTableName());
             $table->double(PlanPriceConstant::AMOUNT);
             $table->enum(PlanPriceConstant::BILLING_CYCLE, BillingCycleEnum::values());
             $table->timestamps();
