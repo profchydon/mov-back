111fef00249b26d28f82e5d2dd0df44b40cece0a
Adu Lawrence <aduaondohemba@gmail.com>
2023-10-05 13:09:46 +0100

    fetch permissions and create user roles

--- app/Http/Controllers/V2/RoleController.php
+++ app/Http/Controllers/V2/RoleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\V2;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRoleRequest;
 use App\Models\Company;
+use App\Repositories\Contracts\PermissionRepositoryInterface;
 use App\Repositories\Contracts\RoleRepositoryInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -11,7 +13,8 @@ use Illuminate\Http\Response;
 class RoleController extends Controller
 {
     public function __construct(
-        private readonly RoleRepositoryInterface $roleRepository
+        private readonly RoleRepositoryInterface $roleRepository,
+        private readonly PermissionRepositoryInterface $permissionRepository
     ) {
     }
 
@@ -20,5 +23,27 @@ class RoleController extends Controller
         $roles = $this->roleRepository->all();
 
         return $this->response(Response::HTTP_OK, __('messages.records-fetched'), $roles);
+    }
+
+    public function fetchPermissions()
+    {
+        $permissions = $this->permissionRepository->all();
+
+        return $this->response(Response::HTTP_OK, __('messages.records-fetched'), $permissions);
+    }
+
+    public function createUserRole(CreateUserRoleRequest $request, Company $company)
+    {
+        $dto = $request->getDTO();
+
+        $dto->setCompanyId($company->id);
+
+        if($this->roleRepository->isRoleExists($dto->getName(), $dto->getCompanyId())){
+            return $this->error(Response::HTTP_BAD_REQUEST, __('messages.role-exists'));
+        }
+
+        $role = $this->roleRepository->createRole($dto);
+
+        return $this->response(Response::HTTP_CREATED, __('messages.role.created'), $role);
     }
 }
--- app/Repositories/Contracts/RoleRepositoryInterface.php
+++ app/Repositories/Contracts/RoleRepositoryInterface.php
@@ -2,6 +2,12 @@
 
 namespace App\Repositories\Contracts;
 
+use App\Domains\DTO\CreateUserRoleDTO;
+use Spatie\Permission\Models\Role;
+
 interface RoleRepositoryInterface extends BaseRepositoryInterface
 {
+    public function createRole(CreateUserRoleDTO $dto): Role;
+    
+    public function isRoleExists(string $name, string $companyId);
 }
