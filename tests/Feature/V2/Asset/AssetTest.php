722de3a1d2ae08a3df9a70d7e09f040bda8f0400
chidi <chidi.nkwocha@rayda.co>
2023-10-07 11:43:10 +0100

    Update AssetTest.php

--- tests/Feature/V2/Asset/AssetTest.php
+++ tests/Feature/V2/Asset/AssetTest.php
@@ -1,15 +1,16 @@
 <?php
 
+use App\Models\Asset;
+use App\Models\Office;
+use App\Models\Company;
+use App\Models\Currency;
+use App\Models\AssetType;
+use Illuminate\Support\Str;
+use Illuminate\Http\Response;
+use App\Repositories\AssetRepository;
 use App\Domains\Constant\AssetConstant;
 use App\Domains\Constant\OfficeConstant;
 use App\Domains\Enum\Asset\AssetStatusEnum;
-use App\Models\Asset;
-use App\Models\AssetType;
-use App\Models\Company;
-use App\Models\Currency;
-use App\Models\Office;
-use App\Repositories\AssetRepository;
-use Illuminate\Http\Response;
 
 beforeEach(function () {
 
@@ -32,14 +33,13 @@ beforeEach(function () {
         AssetConstant::TYPE_ID => $this->asset_type->id,
         AssetConstant::MAKE => $this->make[array_rand($this->make)],
         AssetConstant::MODEL => $this->model[array_rand($this->model)],
-        AssetConstant::SERIAL_NUMBER => "HDGE-A232",
+        AssetConstant::SERIAL_NUMBER => Str::random(10),
         AssetConstant::OFFICE_ID => $this->office->id,
         AssetConstant::PURCHASE_PRICE => "300000.00",
         AssetConstant::CURRENCY => $this->currency->code,
         AssetConstant::ADDED_AT => now(),
         AssetConstant::STATUS => AssetStatusEnum::AVAILABLE->value
     ];
-
 });
 
 test('create single asset', function () {
@@ -52,19 +52,19 @@ test('create single asset', function () {
     expect($response->getData()->message)->toBe('Record created successfully');
     // return $response->getData();
 
- });
+});
 
- test('error when wrong company id is provided', function()  {
+test('error when wrong company id is provided', function () {
     $response = $this->post("http://localhost:80/api/v2/companies/wrong-company-id/assets", $this->payload);
     $response->assertStatus(Response::HTTP_NOT_FOUND);
     $this->assertDatabaseMissing('assets', $this->payload);
     $this->assertDatabaseCount('assets', 0);
- });
+});
 
- test('error when payload is empty', function()  {
+test('error when payload is empty', function () {
     $response = $this->post("http://localhost:80/api/v2/companies/{$this->company->id}/assets", []);
     $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
     expect($response->getData()->success)->toBeFalse();
     expect($response->getData()->message)->toBe('Validation error');
     $this->assertDatabaseCount('assets', 0);
- });
+});
