4e6f3781b4928a5560c675356ce7af5fa6154041
Adu Lawrence <aduaondohemba@gmail.com>
2023-10-18 17:00:05 +0100

    migration for tags

---
+++ app/Domains/Constant/TagConstant.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Domains\Constant;
+
+use App\Domains\Enum\TagStatusEnum;
+
+class TagConstant
+{
+    public const ID = 'id';
+    public const TENANT_ID = 'tenant_id';
+    public const COMPANY_ID = 'company_id';
+    public const NAME = 'name';
+    public const STATUS = 'status';
+
+    public const CREATED_AT = 'created_at';
+    public const UPDATED_AT = 'updated_at';
+    public const DELETED_AT = 'deleted_at';
+
+
+    public const STATUS_ENUM = [
+        TagStatusEnum::ACTIVE,
+        TagStatusEnum::INACTIVE,
+    ];
+}
