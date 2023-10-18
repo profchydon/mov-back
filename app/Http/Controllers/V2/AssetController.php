527425031cc1354c2ef074792ed1ac5dadd19dcd
Adu Lawrence <aduaondohemba@gmail.com>
2023-10-18 09:13:03 +0100

    add asset info

--- app/Http/Controllers/V2/AssetController.php
+++ app/Http/Controllers/V2/AssetController.php
@@ -176,12 +176,15 @@ class AssetController extends Controller
         $request->validate([
             'make' => ['nullable', new HumanNameRule()],
             'model' => ['nullable', new HumanNameRule()],
-            'type_id' => ['required', Rule::exists('asset_types', 'id')],
-            'serial_number' => 'required|string',
-            'purchase_price' => ['required', 'decimal:2,4'],
+            'type_id' => ['nullable', Rule::exists('asset_types', 'id')],
+            'serial_number' => 'nullable|string',
+            'purchase_price' => ['nullable', 'decimal:2,4'],
             'purchase_date' => 'nullable|date',
-            'office_id' => ['required', Rule::exists('offices', 'id')],
-            'currency' => ['required', Rule::exists('currencies', 'code')],
+            'office_id' => ['nullable', Rule::exists('offices', 'id')],
+            'currency' => ['nullable', Rule::exists('currencies', 'code')],
+            'acquisition_type' => ['nullable', 'string'],
+            'custom_tags' => ['nullable', 'array'],
+            'vendor_id' => ['nullable', Rule::exists('vendors', 'id')],
         ]);
 
         $dto = new CreateAssetDTO();
