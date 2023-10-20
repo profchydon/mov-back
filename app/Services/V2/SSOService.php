165373c20385cf816d9a3f33e5be255078f7b22d
chidi <chidi.nkwocha@rayda.co>
2023-10-20 14:39:39 +0100

    Update SSOService.php

--- app/Services/V2/SSOService.php
+++ app/Services/V2/SSOService.php
@@ -66,37 +66,29 @@ class SSOService implements SSOServiceInterface
 
     public function verifyOTP(VerifyOTPDTO $dto)
     {
-        // $user = $this->userRepository->firstWithRelation(UserConstant::ID, $dto->getUserId(), ['otp']);
-
-        // if (!$user || !$user->otp) {
-        //     return false;
-        // }
-
-        // $url = sprintf('%s/api/v1/otp/%s/verify', env('SSO_URL'), $user->otp->sso_id);
-
-        // $data = ['otp' => $dto->getOTP()];
-
-        // $resp = Http::acceptJson()->put($url, $data);
-
-        // if ($resp->status() == Response::HTTP_OK) {
-        //     $user->otp->delete();
-
-        //     $user->update([
-        //         UserConstant::STAGE => UserStageEnum::COMPANY_DETAILS->value,
-        //     ]);
-
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-
         $user = $this->userRepository->firstWithRelation(UserConstant::ID, $dto->getUserId(), ['otp']);
 
-        $user->update([
-            UserConstant::STAGE => UserStageEnum::COMPANY_DETAILS->value,
-        ]);
+        if (!$user || !$user->otp) {
+            return false;
+        }
 
-        return true;
+        $url = sprintf('%s/api/v1/otp/%s/verify', env('SSO_URL'), $user->otp->sso_id);
+
+        $data = ['otp' => $dto->getOTP()];
+
+        $resp = Http::acceptJson()->put($url, $data);
+
+        if ($resp->status() == Response::HTTP_OK) {
+            $user->otp->delete();
+
+            $user->update([
+                UserConstant::STAGE => UserStageEnum::COMPANY_DETAILS->value,
+            ]);
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function updateCompany(AddCompanyDetailsDTO $dto, string $ssoCompanyId)
