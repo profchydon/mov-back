25d9112ad59076dafd9f0c311c4cd8377a8bcbb5
chidi <chidi.nkwocha@rayda.co>
2023-10-22 01:30:07 +0100

    Update permission seeder

--- app/Domains/Auth/PermissionTypes.php
+++ app/Domains/Auth/PermissionTypes.php
@@ -8,21 +8,28 @@ enum PermissionTypes: string
     case ASSET_FULL_ACCESS = 'AssetFullAccess';
     case ASSET_CREATE_ACCESS = 'AssetCreateAccess';
     case ASSET_READ_ACCESS = 'AssetReadAccess';
+    case ASSET_UPDATE_ACCESS = 'AssetUpdateAccess';
+    case ASSET_DELETE_ACCESS = 'AssetDeleteAccess';
     case ASSET_TRANSFER_ACCESS = 'AssetTransferAccess';
 
     // Billing
     case BILLING_FULL_ACCESS = 'BillingFullAccess';
-    case BILLING_READ_ACCESS = 'BillingReadAccess';
     case BILLING_CREATE_ACCESS = 'BillingCreateAccess';
+    case BILLING_READ_ACCESS = 'BillingReadAccess';
+    case BILLING_UPDATE_ACCESS = 'BillingUpdateAccess';
+    case BILLING_DELETE_ACCESS = 'BillingDeleteAccess';
 
     // Account
     case ACCOUNT_FULL_ACCESS = 'AccountFullAccess';
     case ACCOUNT_READ_ACCESS = 'AccountReadAccess';
     case ACCOUNT_CREATE_ACCESS = 'AccountCreateAccess';
+    case ACCOUNT_UPDATE_ACCESS = 'AccountUpdateAccess';
+    case ACCOUNT_DELETE_ACCESS = 'AccountDeleteAccess';
 
     // Role
-
     case ROLE_FULL_ACCESS = 'RoleFullAccess';
     case ROLE_CREATE_ACCESS = 'RoleCreateAccess';
     case ROLE_READ_ACCESS = 'RoleReadAccess';
+    case ROLE_DELETE_ACCESS = 'RoleDeleteAccess';
+    case ROLE_UPDATE_ACCESS = 'RoleUpdateAccess';
 }
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -16,14 +16,22 @@ class PermissionSeeder extends Seeder
         $permissions = collect([
             ['name' => PermissionTypes::ASSET_FULL_ACCESS, 'display' => 'Full Access to Assets'],
             ['name' => PermissionTypes::ASSET_CREATE_ACCESS, 'display' => 'Permission to Create Asset'],
-            ['name' => PermissionTypes::ASSET_READ_ACCESS, 'display' => 'Permission to Read Asset'],
+            ['name' => PermissionTypes::ASSET_READ_ACCESS, 'display' => 'Permission to View Asset'],
             ['name' => PermissionTypes::ASSET_TRANSFER_ACCESS, 'display' => 'Permission to Transfer Asset'],
+
             ['name' => PermissionTypes::BILLING_FULL_ACCESS, 'display' => 'Full Access to Billing'],
             ['name' => PermissionTypes::BILLING_CREATE_ACCESS, 'display' => 'Permission to Create Billing'],
-            ['name' => PermissionTypes::BILLING_READ_ACCESS, 'display' => 'Permission to Read Billing'],
-            ['name' => PermissionTypes::ACCOUNT_FULL_ACCESS, 'display' => 'Full Access to Accounts'],
-            ['name' => PermissionTypes::ACCOUNT_CREATE_ACCESS, 'display' => 'Permission to Create Accounts'],
-            ['name' => PermissionTypes::ACCOUNT_READ_ACCESS, 'display' => 'Permission to Read Accounts'],
+            ['name' => PermissionTypes::BILLING_READ_ACCESS, 'display' => 'Permission to View Billing'],
+
+            ['name' => PermissionTypes::ACCOUNT_FULL_ACCESS, 'display' => 'Full Access to Users'],
+            ['name' => PermissionTypes::ACCOUNT_CREATE_ACCESS, 'display' => 'Permission to Create Users'],
+            ['name' => PermissionTypes::ACCOUNT_READ_ACCESS, 'display' => 'Permission to View Users'],
+            ['name' => PermissionTypes::ACCOUNT_DELETE_ACCESS, 'display' => 'Permission to Delete Users'],
+
+            ['name' => PermissionTypes::ROLE_FULL_ACCESS, 'display' => 'Full Access to Roles'],
+            ['name' => PermissionTypes::ROLE_CREATE_ACCESS, 'display' => 'Permission to Create Roles'],
+            ['name' => PermissionTypes::ROLE_READ_ACCESS, 'display' => 'Permission to View Roles'],
+            ['name' => PermissionTypes::ROLE_DELETE_ACCESS, 'display' => 'Permission to Delete Roles'],
         ]);
 
         $permissions->each(function ($permission) {
