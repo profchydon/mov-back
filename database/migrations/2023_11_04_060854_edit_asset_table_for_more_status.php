8f983801e5bd190bc2412e4663993689b3ae8808
Bello Opeyemi <opybee07@gmail.com>
2023-11-04 09:18:59 +0100

    Add test

--- database/migrations/2023_11_04_060854_edit_asset_table_for_more_status.php
+++ database/migrations/2023_11_04_060854_edit_asset_table_for_more_status.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        return;
+
         $enumValues = "'" . \Illuminate\Support\Arr::join(\App\Domains\Enum\Asset\AssetStatusEnum::values(), "','") . "'";
 
         \Illuminate\Support\Facades\DB::statement("ALTER TABLE assets MODIFY status ENUM({$enumValues})");
