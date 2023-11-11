fab008884687ca2bceb762f73cbd624b66c0de48
chidi <chidi.nkwocha@rayda.co>
2023-11-11 19:15:51 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,12 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // $this->call(PermissionSeeder::class);
-        // $this->call(RoleSeeder::class);
-        // $this->call(PlanSeeder::class);
-        // $this->call(CountrySeeder::class);
-        // $this->call(CurrencySeeder::class);
-        // $this->call(FeatureSeeder::class);
+        $this->call(PermissionSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(PlanSeeder::class);
+        $this->call(CountrySeeder::class);
+        $this->call(CurrencySeeder::class);
+        $this->call(FeatureSeeder::class);
         // $this->call(AssetTypeSeeder::class);
         // // $this->call(CompanySeeder::class);
         // $this->call(InvoiceSeeder::class);
