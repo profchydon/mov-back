3c7dd74140b63f7fb6311e041576e7db4d337e32
Bello Opeyemi <opybee07@gmail.com>
2023-11-27 08:52:32 +0100

    CRUD on seats

---
+++ app/Http/Controllers/V2/SeatController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\V2;
+
+use App\Domains\Constant\UserCompanyConstant;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RevokeSeatFromUserRequest;
+use App\Http\Requests\User\AssignSeatToUserRequest;
+use App\Models\Company;
+use App\Models\User;
+use App\Models\UserCompany;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class SeatController extends Controller
+{
+    public function index(Company $company, Request $request)
+    {
+        $users = $company->usersWithSeats()->simplePaginate();
+
+        return $this->response(Response::HTTP_OK, __('messages.records-fetched'), $users);
+    }
+
+    public function delete(RevokeSeatFromUserRequest $request, Company $company)
+    {
+        $user_companies = collect($request->user_ids)->map(fn($user_id) => UserCompany::where('user_id', $user_id)->where('company_id', $company->id)->first());
+        $user_companies->each(fn($relation) => $relation->revokeSeat());
+
+        return $this->response(Response::HTTP_OK, __('messages.records-updated'), $user_companies);
+    }
+
+    public function store(AssignSeatToUserRequest $request, Company $company)
+    {
+        $user_companies = collect($request->user_ids)->map(fn($user_id) => UserCompany::where('user_id', $user_id)->where('company_id', $company->id)->first());
+        $user_companies->each(fn($relation) => $relation->assignSeat());
+
+        return $this->response(Response::HTTP_OK, __('messages.records-updated'), $user_companies);
+    }
+}
--- app/Models/UserCompany.php
+++ app/Models/UserCompany.php
@@ -8,10 +8,16 @@ use App\Traits\HasCompany;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
 class UserCompany extends Model
 {
     use HasFactory, HasUuids, HasCompany;
+
+    public function scopeWithSeats(Builder $query)
+    {
+        $query->where(UserCompanyConstant::HAS_SEAT, true);
+    }
 
     protected $guarded = [
         UserCompanyConstant::ID,
@@ -30,5 +36,19 @@ class UserCompany extends Model
     public function user()
     {
         return $this->belongsTo(User::class, UserConstant::USER_ID);
+    }
+
+    public function assignSeat()
+    {
+        return $this->update([
+            UserCompanyConstant::HAS_SEAT => true
+        ]);
+    }
+
+    public function revokeSeat()
+    {
+        return $this->update([
+            UserCompanyConstant::HAS_SEAT => false
+        ]);
     }
 }
