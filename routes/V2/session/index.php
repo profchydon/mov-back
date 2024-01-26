b44e3ef086a7c8ab9482ef35ffc5eba918646010
chidi <chidi.nkwocha@rayda.co>
2024-01-26 09:49:11 +0100

    Update index.php

--- routes/V2/session/index.php
+++ routes/V2/session/index.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\V2\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['token.decrypt', 'payload.decrypt'])->controller(SessionController::class)->prefix('sessions')->group(function () {
+Route::controller(SessionController::class)->prefix('sessions')->group(function () {
     Route::get('authorization', [SessionController::class, 'authorization'])->name('login.link');
-    Route::post('confirmation', [SessionController::class, 'confirmation']);
+    Route::post('confirmation', [SessionController::class, 'confirmation'])->middleware(['payload.decrypt']);
 });
