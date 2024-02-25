99d614df673e8cab78022a4aae46baadd5c1157b
chidi <chidi.nkwocha@rayda.co>
2024-02-25 18:46:43 +0100

    Update subscription-upgraded.blade.php

--- resources/views/emails/subscription/subscription-upgraded.blade.php
+++ resources/views/emails/subscription/subscription-upgraded.blade.php
@@ -8,12 +8,12 @@ Subscription Upgraded
 
 <div style="background: linear-gradient(to top, transparent, #C0C0C040); padding: 2px 30px;">
 
-    <div style="text-align: center; padding: 40px 0px;">
-      <img src="https://s3.amazonaws.com/rayda.co/images+for+email+template/plane.png" alt=""
-        style="display: inline-block; max-width: 40%; height: auto;">
-    </div>
+  <div style="text-align: center; padding: 40px 0px;">
+    <img src="https://s3.amazonaws.com/rayda.co/images+for+email+template/plane.png" alt=""
+      style="display: inline-block; max-width: 40%; height: auto;">
+  </div>
 
-    <div style="
+  <div style="
       color: #191919;
       font-family: 'IBM Plex Sans', sans-serif;
       background: #fff;
@@ -23,52 +23,54 @@ Subscription Upgraded
       width: auto;
       margin-bottom: 10px;">
 
-      <p
-        style="font-size: 30px; line-height: 40px; color: #191919; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400; margin: 0px;">
-        Your Plan has been <br> upgraded successfully 👍
-      </p>
+    <p
+      style="font-size: 30px; line-height: 40px; color: #191919; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400; margin: 0px;">
+      Your Plan has been <br> upgraded successfully 👍
+    </p>
+
+    <p
+      style="font-size: 14px; line-height: 24px; color: #191919; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400;">
+      Hello <b>{{ $company?->name }}</b> 👋️</p>
+    <br>
+    <p
+      style="font-size: 14px; line-height: 24px; color: #191919; font-family: 'IBM Plex Sans', sans-serif; font-weight: 300;">
+      Your Rayda account has been upgraded successfully from <b> {{ strtolower($oldPlan?->name) }} plan </b> to <b> {{
+        strtolower($newPlan?->name) }} plan </b>. This means you
+      have gained access to the following benefits:
+    </p>
+
+    <ul style="list-style-type: none; font-family: 'IBM Plex Sans', sans-serif; font-size: 16px; padding: 0px;">
+
+      @foreach ($offers as $offer)
+
+      <li style="display: flex; font-weight: 500; margin: 20px 0px;">
+        <img src="https://s3.amazonaws.com/rayda.co/images+for+email+template/check-verified.png" class="object-contain"
+          alt=""
+          style="display: flex; margin-right: 10px; width: 20px; height: 20px; max-width: 20px; max-height: 20px; position: relative; top: 2px" />
+        {{ $offer }}
+      </li>
+
+      @endforeach
+
+    </ul>
+
+    <div style="margin-top: 50px;">
 
       <p
-        style="font-size: 14px; line-height: 24px; color: #191919; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400;">
-        Hello <b>{{ $company?->name }}</b> 👋️</p>
-        <br>
-      <p
-        style="font-size: 14px; line-height: 24px; color: #191919; font-family: 'IBM Plex Sans', sans-serif; font-weight: 300;">
-        Your Rayda account has been upgraded successfully from <b> {{ strtolower($oldPlan?->name) }} plan </b> to <b> {{ strtolower($newPlan?->name) }} plan </b>. This means you
-        have gained access to the following benefits:
-      </p>
-
-      <ul style="list-style-type: none; font-family: 'IBM Plex Sans', sans-serif; font-size: 16px; padding: 0px;">
-
-        @foreach ($offers as $offer)
-
-            <li style="display: flex; font-weight: 500; margin: 20px 0px;">
-                <img src="https://s3.amazonaws.com/rayda.co/images+for+email+template/check-verified.png"
-                class="object-contain" alt=""
-                style="display: flex; margin-right: 10px; width: 20px; height: 20px; max-width: 20px; max-height: 20px; position: relative; top: 2px" />
-                {{ $offer }}
-            </li>
-
-        @endforeach
-
-      </ul>
-
-      <div style="margin-top: 50px;">
-
-        <p
         style="font-size: 14px; line-height: 10px; color: #191919; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400;">
         <b>Order details:</b>
-        </p>
-        <p
-          style="font-size: 14px; line-height: 10px; color: #004CCC; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400;">
-          <span style="font-weight: 300; color: #191919;">Invoice No:</span> <a href={{ $link }}> {{ $invoice?->invoice_number}} </a>
-        </p>
+      </p>
+      <p
+        style="font-size: 14px; line-height: 10px; color: #004CCC; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400;">
+        <span style="font-weight: 300; color: #191919;">Invoice No:</span> <a href={{ $link }}> {{
+          $invoice?->invoice_number}} </a>
+      </p>
 
-      </div>
+    </div>
 
-      <hr style="border: 0.5px solid #DCDFEA; margin: 30px 0px;">
+    {{-- <hr style="border: 0.5px solid #DCDFEA; margin: 30px 0px;">
 
-      <div style="
+    <div style="
       display: flex;
       flex-direction: column;
       color: #191919;
@@ -114,17 +116,17 @@ Subscription Upgraded
         </div>
       </div>
 
-    </div>
-
-    </div>
-
-    <p
-      style="font-size: 14px; line-height: 24px; color: #404968; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400; padding: 0px 20px;">
-      Learn more about our plans, payments accepted and billing practices in our help centre.
-      <br> <br> <br>
-      The Rayda Team
-    </p>
+    </div> --}}
 
   </div>
+
+  <p
+    style="font-size: 14px; line-height: 24px; color: #404968; font-family: 'IBM Plex Sans', sans-serif; font-weight: 400; padding: 0px 20px;">
+    Learn more about our plans, payments accepted and billing practices in our help centre.
+    <br> <br> <br>
+    The Rayda Team
+  </p>
+
+</div>
 
 @endsection
