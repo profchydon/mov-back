731e50be867d8221bdd9352c6b2cea37ec6443cd
Bello Opeyemi <opybee07@gmail.com>
2024-03-01 09:37:54 +0100

    Fix user id eerror

--- routes/V2/documents/index.php
+++ routes/V2/documents/index.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => [], 'prefix' => 'companies/{company}'], function(){
+Route::group(['middleware' => ['token.decrypt', 'auth:sanctum',  'user-in-company'], 'prefix' => 'companies/{company}'], function(){
     Route::resource('documents', \App\Http\Controllers\V2\DocumentController::class);
     Route::post('/documents/{document}/change-file', [\App\Http\Controllers\V2\DocumentController::class, 'changeFile']);
     Route::post('documents/{document}/assets', [\App\Http\Controllers\V2\DocumentController::class, 'addAssets']);
@@ -10,4 +10,3 @@ Route::group(['middleware' => [], 'prefix' => 'companies/{company}'], function()
     Route::get('document-types', [\App\Http\Controllers\V2\DocumentController::class, 'getDocumentType']);
     Route::delete('document-types/{type}', [\App\Http\Controllers\V2\DocumentController::class, 'deleteDocumentType']);
 });
-//'token.decrypt', 'auth:sanctum',  'user-in-company'
