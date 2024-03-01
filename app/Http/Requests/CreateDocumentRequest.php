02f43a3e3908d7b206bd775cf324da4d2cd5ea8d
Bello Opeyemi <opybee07@gmail.com>
2024-03-01 16:45:57 +0100

    Select type

--- app/Http/Requests/CreateDocumentRequest.php
+++ app/Http/Requests/CreateDocumentRequest.php
@@ -15,7 +15,7 @@ class CreateDocumentRequest extends FormRequest
     {
         return [
             'name' => ['required', Rule::unique('documents', 'name')->where(DocumentConstant::COMPANY_ID, $this->company?->id)->whereNull('deleted_at')],
-            'type' => ['required', new HumanNameRule(), Rule::exists('document_types', 'name')],
+            'type' => ['required', new HumanNameRule()],
             'registration_date' => ['sometimes', 'nullable', 'date_format:Y-m-d'],
             'expiration_date' => [Rule::when(
                 fn($input) => !empty($this->input('registration_date')),
