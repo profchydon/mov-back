e74bcb6b6e9d64df44b7024dd1540e20df772065
Olaifa Darasimi <darasimiolaifa@gmail.com>
2024-06-07 16:07:02 +0100

    fix the asset assignment bug

--- app/Http/Controllers/V2/AssetController.php
+++ app/Http/Controllers/V2/AssetController.php
@@ -201,7 +201,7 @@ class AssetController extends Controller
         });
 
         if($rejectedAssets->count() > 0) {
-            return $this->error(Response::HTTP_BAD_REQUEST, __('{$rejectedAssets->count()} assets cannot be assigned because assignee belongs to a different company. Kindly assign manually.'), $rejectedAssets);
+            return $this->error(Response::HTTP_BAD_REQUEST, __("{$rejectedAssets->count()} assets cannot be assigned because assignee belongs to a different company. Kindly assign manually."), $rejectedAssets);
         }
 
         return $this->response(Response::HTTP_ACCEPTED, __("{$assets->count()} assets created"), $assets);
